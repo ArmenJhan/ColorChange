ce5944f689a0346c7f9a8170a179acdcd679f1e5
Armen <madarmen1987@gmail.com>
2022-09-23 23:43:13 +0300

    Work with UI and RGB sliders

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redColorView: UILabel!
+    @IBOutlet var greenColorView: UILabel!
+    @IBOutlet var blueColorView: UILabel!
+    
+    var redValue: CGFloat = 0
+    var greenValue: CGFloat = 0
+    var blueValue: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
+    
+   
 
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        redColorView.text = String(format: "%.2f", sender.value)
+       
+        
+    }
+    
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        greenColorView.text = String(format: "%.2f", sender.value)
+        
+    }
+    
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+        blueColorView.text = String(format: "%.2f", sender.value)
 
+    }
 }
-
